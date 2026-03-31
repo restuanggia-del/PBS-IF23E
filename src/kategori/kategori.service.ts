55fcbe5774f8452b91ee324fbd2367d79791ce84
restuanggia-del <241223303+restuanggia-del@users.noreply.github.com>
2026-03-31 08:47:56 +0700

    tambah filter nama

--- src/kategori/kategori.service.ts
+++ src/kategori/kategori.service.ts
@@ -1,27 +1,55 @@
-import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateKategoriDto } from './dto/create-kategori.dto';
 import { UpdateKategoriDto } from './dto/update-kategori.dto';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class KategoriService {
   // buat konstruktor untuk menginisialisasi PrismaService
   constructor(private readonly prisma: PrismaService) {}
 
-  // buat fungsi tambah data
+  //buat fungsi untuk tambah data
   async create(createKategoriDto: CreateKategoriDto) {
-    // return 'This action adds a new kategori';
-    // simpan data kategori
-    await this.prisma.kategori.create({
-      data: {
-        nama: createKategoriDto.nama,
+    //return 'This action adds a new kategori';
+
+    //buat variabel untuk filter nama kategori, agar tidak terjadi duplikasi data kategori
+    const nama_filter = createKategoriDto.nama
+      .trim() //spasi di awal dan akhir kata akan dihapus
+      .replace(/\s/g, '') //spasi di tengah kata akan dihapus
+      .toLowerCase(); //merubah huruf kecil semua
+
+    //cek apakah data kategori sudah ada
+    const exist = await this.prisma.kategori.findFirst({
+      where: {
+        nama_filter: nama_filter,
       },
     });
 
-    // tampikan respon
+    //jika data kategori sudah ada
+    if (exist) {
+      throw new ConflictException({
+        success: false,
+        massage: 'Data kategori sudah ada!',
+        metadata: {
+          status: HttpStatus.CONFLICT,
+        },
+      });
+    }
+
+    //jika data kategori belum ada
+
+    //simpan data kategori
+    await this.prisma.kategori.create({
+      data: {
+        nama: createKategoriDto.nama,
+        nama_filter: nama_filter,
+      },
+    });
+
+    //tampilkan respon
     return {
       success: true,
-      message: 'Data Kategori Berhasil Disimpan',
+      message: 'Data kategori berhasil ditambahkan!',
       metadata: {
         status: HttpStatus.CREATED,
       },
@@ -70,8 +98,34 @@ export class KategoriService {
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} kategori`;
+  //buat fungsi untuk detail data
+  async findOne(id: number) {
+    //return `This action returns a #${id} kategori`;
+
+    // tampilkan data kategori sesuai id
+    const data = await this.prisma.kategori.findUnique({
+      where: { id: id },
+    });
+
+    // jika data ketegori tidak ditemukan
+    if (!data) {
+      throw new NotFoundException({
+        success: false,
+        message: 'Data kategori tidak ditemukan!',
+        metadata: {
+          status: HttpStatus.NOT_FOUND,
+        },
+      });
+    }
+    // jika data kategori ditemukan
+    return {
+      success: true,
+      message: '',
+      metadata: {
+        status: HttpStatus.OK,
+      },
+      data: data,
+    };
   }
 
   update(id: number, updateKategoriDto: UpdateKategoriDto) {
